bf31f7aa6f9fb88f6ff79184a0f526bf20223822
Alok Singh <alokbeniwal@gmail.com>
2017-10-03 09:58:15 -0700

    cleanup code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 from argparse import ArgumentParser
 
@@ -11,11 +14,13 @@ import torch.nn.functional as nn
 import torch.optim as optim
 from tensorflow.examples.tutorials.mnist import input_data
 from torch.autograd import Variable
+from torch.nn import BatchNorm1d, Linear, ReLU, Sequential, Sigmoid
+from torch.optim import Adam
 
 ITERS = 1000000  # number of training steps (for the generator)
 
 parser = ArgumentParser()
-parser.add_argument('--render', type=bool, action='store_true')
+parser.add_argument('--render', action='store_true')
 parser.add_argument('--iterations', '-n', type=int, default=ITERS)
 parser.add_argument('--batch_size', type=int, default=128)
 args = parser.parse_args()
@@ -30,17 +35,17 @@ hidden_dim = 128
 d_step = 3  # discriminator takes 3 steps for every 1 the generator does.
 lr = 1e-3
 
-G = torch.nn.Sequential(
-    torch.nn.Linear(z_dim, hidden_dim),
-    torch.nn.ReLU(),
-    torch.nn.Linear(hidden_dim, X_dim),
-    torch.nn.Sigmoid(),
+G = Sequential(
+    Linear(z_dim, hidden_dim),
+    ReLU(),
+    Linear(hidden_dim, X_dim),
+    Sigmoid(),
 ).cuda()
 
-D = torch.nn.Sequential(
-    torch.nn.Linear(X_dim, hidden_dim),
-    torch.nn.ReLU(),
-    torch.nn.Linear(hidden_dim, 1),
+D = Sequential(
+    Linear(X_dim, hidden_dim),
+    ReLU(),
+    Linear(hidden_dim, 1),
 ).cuda()
 
 
@@ -49,64 +54,65 @@ def reset_grads():
     D.zero_grad()
 
 
-G_optim = optim.Adam(G.parameters(), lr=lr)
-D_optim = optim.Adam(D.parameters(), lr=lr)
+G_optim = Adam(G.parameters(), lr=lr)
+D_optim = Adam(D.parameters(), lr=lr)
 
-for iter in range(args.iterations):
-    #
-    for _ in range(d_step):
-        # Sample data
+if __name__ == '__main__':
+    for iter in range(args.iterations):
+        #
+        for _ in range(d_step):
+            # Sample data
+            z = Variable(torch.randn(BATCH_SIZE, z_dim)).cuda()
+            X = torch.from_numpy(mnist.train.next_batch(BATCH_SIZE)[0]).pin_memory()
+            X = Variable(X).cuda()
+
+            # Discriminator
+            G_sample = G(z)
+            D_real = D(X)
+            D_fake = D(G_sample)
+
+            D_loss = 0.5 * (torch.mean((D_real - 1) ** 2) + torch.mean(D_fake ** 2))
+
+            D_loss.backward()
+            D_optim.step()
+            reset_grads()
+
+        # Generator
         z = Variable(torch.randn(BATCH_SIZE, z_dim)).cuda()
-        X = torch.from_numpy(mnist.train.next_batch(BATCH_SIZE)[0]).pin_memory()
-        X = Variable(X).cuda()
 
-        # Discriminator
         G_sample = G(z)
-        D_real = D(X)
         D_fake = D(G_sample)
 
-        D_loss = 0.5 * (torch.mean((D_real - 1) ** 2) + torch.mean(D_fake ** 2))
+        G_loss = 0.5 * torch.mean((D_fake - 1) ** 2)
 
-        D_loss.backward()
-        D_optim.step()
+        G_loss.backward()
+        G_optim.step()
         reset_grads()
 
-    # Generator
-    z = Variable(torch.randn(BATCH_SIZE, z_dim)).cuda()
+        # Print and plot every now and then
+        if iter % 1000 == 0:
+            print(
+                'i: {}'.format(iter),
+                'D: {:.4}'.format(D_loss.data[0]),
+                'G: {:.4}'.format(G_loss.data[0]),
+                sep='\n',
+            )
 
-    G_sample = G(z)
-    D_fake = D(G_sample)
+            samples = G(z).cpu().data.numpy()[:16]
 
-    G_loss = 0.5 * torch.mean((D_fake - 1) ** 2)
+            fig = plt.figure(figsize=(4, 4))
+            gs = gridspec.GridSpec(4, 4)
+            gs.update(wspace=0.05, hspace=0.05)
 
-    G_loss.backward()
-    G_optim.step()
-    reset_grads()
+            for i, sample in enumerate(samples):
+                ax = plt.subplot(gs[i])
+                plt.axis('off')
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
 
-    # Print and plot every now and then
-    if iter % 1000 == 0:
-        print(
-            'i: {}'.format(iter),
-            'D: {:.4}'.format(D_loss.data[0]),
-            'G: {:.4}'.format(G_loss.data[0]),
-            sep='\n',
-        )
+            if not os.path.exists('imgs/'):
+                os.makedirs('imgs/')
 
-        samples = G(z).cpu().data.numpy()[:16]
-
-        fig = plt.figure(figsize=(4, 4))
-        gs = gridspec.GridSpec(4, 4)
-        gs.update(wspace=0.05, hspace=0.05)
-
-        for i, sample in enumerate(samples):
-            ax = plt.subplot(gs[i])
-            plt.axis('off')
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-            ax.set_aspect('equal')
-
-        if not os.path.exists('imgs/'):
-            os.makedirs('imgs/')
-
-        plt.savefig('imgs/{}.png'.format(str(iter)), bbox_inches='tight')
-        plt.close(fig)
+            plt.savefig('imgs/{}.png'.format(str(iter)), bbox_inches='tight')
+            plt.close(fig)
